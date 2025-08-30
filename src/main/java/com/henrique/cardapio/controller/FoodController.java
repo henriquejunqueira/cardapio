958c05680f117e4b201b8eb3a095dffeffb15503
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-08-30 10:32:47 -0300

    Configured the jpa and postgresql dependencies in the pom.xml file

--- src/main/java/com/henrique/cardapio/controller/FoodController.java
+++ src/main/java/com/henrique/cardapio/controller/FoodController.java
@@ -1,5 +1,15 @@
 package com.henrique.cardapio.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Indicates to spring that this class is a controller
+@RequestMapping("food") // Indicates to spring the endpoint of this controller
 public class FoodController {
 
+    @GetMapping // Endpoint of this method
+    public void getAll(){ // Method to return all foods registered in the database
+
+    }
 }
